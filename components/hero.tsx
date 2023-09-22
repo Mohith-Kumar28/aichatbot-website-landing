3d4802cc138c927fb0c8801f6b420f5dc0cbb613
Mohith-Kumar28 <mohithkumar808@gmail.com>
2023-09-22 17:17:23 +0530

    testing

--- components/hero.tsx
+++ components/hero.tsx
@@ -29,7 +29,7 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="max-w-3xl  mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 text-white mb-4" data-aos="fade-up">Revolutionize Customer Engagement with Our AI Powered Chatbot</h1>
+            <h1 className="h1 text-white mb-4" data-aos="fade-up">Revolutionize Customer Engagement with Our AI Powered Chatbot.</h1>
             <p className="text-xl text-gray-400 font-extrabold mb-8 max-w-[55%] mx-auto" data-aos="fade-up" data-aos-delay="200">Deliver Instant Support, Boost Efficiency, and Enhance User Experiences.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
